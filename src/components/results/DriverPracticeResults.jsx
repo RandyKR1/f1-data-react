fa15617c121697ea3d2d1fe471d1d4d1ab69337c
Randall Reichenberger <randall.reichenberger1@gmail.com>
2025-06-23 18:22:42 -0500

    updated layout

--- src/components/results/DriverPracticeResults.jsx
+++ src/components/results/DriverPracticeResults.jsx
@@ -8,32 +8,42 @@ import {
     getMeetings,
     getPosition,
 } from "../../api";
-import { getDriverFinalPosition } from "../../utilities";
+import { getDriverFinalPosition, getMinBy } from "../../utilities";
 
 const DriverPracticeResults = () => {
-
     const { sessionKey, driver_number } = useParams();
     const [drivers, setDrivers] = useState([]);
-    const [laps, setLaps] = useState([])
+    const [laps, setLaps] = useState([]);
     const [stints, setStints] = useState([]);
     const [loading, setLoading] = useState(true);
     const [sessionInfo, setSessionInfo] = useState(null);
-    const [meetingInfo, setMeetingInfo] = useState(null)
+    const [meetingInfo, setMeetingInfo] = useState(null);
     const [positionInfo, setPositionInfo] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
-            const [lapData, stintData, driverData, positionData, sessionData, meetingData] = await Promise.all([
+            const [
+                lapData,
+                stintData,
+                driverData,
+                positionData,
+                sessionData,
+                meetingData,
+            ] = await Promise.all([
                 getLaps(sessionKey),
                 getStints(sessionKey),
                 getDrivers(sessionKey),
                 getPosition(sessionKey),
                 getSessions(),
-                getMeetings()
+                getMeetings(),
             ]);
-            const foundSession = sessionData.find((s) => s.session_key.toString() === sessionKey);
-            const foundMeeting = meetingData.find((m) => m.meeting_key === foundSession.meeting_key);
+            const foundSession = sessionData.find(
+                (s) => s.session_key.toString() === sessionKey
+            );
+            const foundMeeting = meetingData.find(
+                (m) => m.meeting_key === foundSession.meeting_key
+            );
 
             setLaps(lapData);
             setStints(stintData);
@@ -51,73 +61,78 @@ const DriverPracticeResults = () => {
         return <div>Loading Results...</div>;
     }
 
-    const driver = drivers.find((d) => d.driver_number.toString() === driver_number);
-    const driverLaps = laps.filter((lap) => lap.driver_number.toString() === driver_number);
-    const driverStints = stints.filter((stint) => stint.driver_number.toString() === driver_number);
-    const driverPosition = positionInfo.find((position) => position.driver_number.toString() === driver_number);
+    const driver = drivers.find(
+        (d) => d.driver_number.toString() === driver_number
+    );
+    if (!driver) {
+        return <div>Driver not found</div>;
+    }
 
-    const bestLap = driverLaps
-        .filter((lap) => lap.lap_duration)
-        .reduce(
-            (best, current) =>
-                !best || current.lap_duration < best.lap_duration
-                    ? current
-                    : best,
-            null
-        );
+    const driverLaps = laps.filter(
+        (lap) => lap.driver_number.toString() === driver_number
+    );
+    const driverStints = stints.filter(
+        (stint) => stint.driver_number.toString() === driver_number
+    );
 
-    const totalLaps = driverLaps.length;
-    const fullName = `${driver.first_name} ${driver.last_name}`
-    const headShot = driver.headshot_url
-    const driverNumber = driver.driver_number
-    const finalPosition = getDriverFinalPosition(driverNumber, positionInfo)
+    const bestLap = getMinBy(
+        driverLaps.filter((lap) => lap.lap_duration != null),
+        "lap_duration"
+    );
 
-    console.log("Session Info", sessionInfo)
-    console.log("Meeting Info", meetingInfo)
-    console.log("Driver", driver)
-    console.log("Lap Data", driverLaps)
-    console.log("Stint Data", driverStints)
-    console.log("Position Data", driverPosition)
-
+    const fullName = `${driver.first_name} ${driver.last_name}`;
+    const headShot = driver.headshot_url;
+    const driverNumber = driver.driver_number;
+    const finalPosition = getDriverFinalPosition(driverNumber, positionInfo);
 
     return (
-        <div>
-            <div className="container vw-100">
-
-
-                <div className="row">
-                    <div className="col-md-4 d-flex flex-column">
-                        <img
-                            src={headShot}
-                            alt=""
-                            style={{ maxWidth: "100px", borderRadius: "12px" }}
-                        />
-                        <p className="fw-bold">{fullName} #{driverNumber}</p>
-                        <p className="fw-bold">{driver.team_name}</p>
-                    </div>
-
-                    <div className="col-md-8 fw-bold d-flex flex-column">
-                        <h3 className="col-sm-12 col-md-6 text-center text">{meetingInfo.meeting_official_name}</h3>
-                        <h3 className="col-md-6 text-center">Free {sessionInfo.session_name} Driver Results</h3>
-                    </div>
+        <div className="container vw-100">
+            <div className="row align-items-center mb-4">
+                <div className="col-md-4 d-flex flex-column align-items-center">
+                    <img
+                        src={headShot}
+                        alt={`${fullName} Headshot`}
+                        style={{ maxWidth: "100px", borderRadius: "12px" }}
+                    />
+                    <p className="fw-bold mt-2">
+                        {fullName} #{driverNumber}
+                    </p>
+                    <p className="fw-bold">{driver.team_name}</p>
                 </div>
 
-
-                <div className="row">
-                    <div className="col-md-4 d-flex flex-column">
-                        <p className="fw-bold">Finishing Position</p>
-                        <p>P{finalPosition}</p>
-                    </div>
-                    <div className="col-md-4">Fastest Lap</div>
-                    <div className="col-md-4">Race Duration</div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">Team Radio</div>
-                    <div class="col-md-6">{driverStints[0].compound}</div>
+                <div className="col-md-8 text-center fw-bold">
+                    <h3>{meetingInfo.meeting_official_name}</h3>
+                    <h4>Free {sessionInfo.session_name} Driver Results</h4>
                 </div>
             </div>
-        </div >
-    )
-}
 
-export default DriverPracticeResults
+            <div className="row text-center mb-4">
+                <div className="col-md-4">
+                    <p className="fw-bold">Finishing Position</p>
+                    <p>P{finalPosition}</p>
+                </div>
+                <div className="col-md-4">
+                    <p className="fw-bold">Fastest Lap</p>
+                    <p>{bestLap ? bestLap.lap_duration : "N/A"}</p>
+                </div>
+                <div className="col-md-4">
+                    <p className="fw-bold">Race Duration</p>
+                    <p>{/* Add race duration here if available */}</p>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <p className="fw-bold">Team Radio</p>
+                    <p>{/* Add team radio info here if available */}</p>
+                </div>
+                <div className="col-md-6">
+                    <p className="fw-bold">Compound</p>
+                    <p>{driverStints.length > 0 ? driverStints[0].compound : "N/A"}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DriverPracticeResults;
